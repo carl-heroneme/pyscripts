56859a8943e6b21d9084e8f5c35e0e249ae04134
carl-heroneme <42617381+carl-heroneme@users.noreply.github.com>
2019-04-01 15:04:55 -0500

    comments

--- raspa_ff_tester.py
+++ raspa_ff_tester.py
@@ -4,7 +4,9 @@ import subprocess
 testset = os.listdir('/home/c662h070/Software/raspa2/share/raspa/molecules/Roeselova_O3') 
 testsplit = [f.split('.')[0] for f in testset]
 print(testsplit)
-
+#remember to edit jobarrays 1-n%i in your submit script to limit the # of runs based on how many FF's you are testing!
+#does bash things. edits each input file and spawns an array of raspa runs for each of your test ff's. 
+#raspajob is your submit script, simulation.input your input file, and component 1 is your solute
 for f in testsplit:
 	subprocess.run(['mkdir '+f], shell=True)
 	subprocess.run(['cp simulation.input raspajob '+f], shell=True)
